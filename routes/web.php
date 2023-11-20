d3010b70db164edcfa8ff9d1555d7506a3716d87
eko-prasetio <caedablerg@gmail.com>
2023-11-20 16:44:37 +0700

    update login for email, name, and phone

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +20,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// ADMIN GROUP MIDDLEWARE
+Route::middleware(['auth', 'roles:admin'])->group(function () {    
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+}); 
