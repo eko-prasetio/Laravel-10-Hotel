0b659b10c9cc668a3447fb5bf02189e69c43a145
eko-prasetio <caedablerg@gmail.com>
2023-11-30 14:47:09 +0700

    add partial profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,5 +27,11 @@ class AdminController extends Controller
 
     public function AdminLogin() {
         return view('admin.admin_login');
+    }
+
+    public function AdminProfile() {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
     }
 }
