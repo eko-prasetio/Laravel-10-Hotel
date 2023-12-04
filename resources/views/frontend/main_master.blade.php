18f2e7938e2768c5e3cc38a868f3fb32c9ebbdfe
eko-prasetio <caedablerg@gmail.com>
2023-12-04 14:02:50 +0700

    update user profile

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -30,6 +30,7 @@
         <link rel="stylesheet" href="{{asset('frontend/assets/css/responsive.css')}}">
         <!-- Theme Dark CSS -->
         <link rel="stylesheet" href="{{asset('frontend/assets/css/theme-dark.css')}}">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{asset('frontend/assets/img/favicon.png')}}">
@@ -90,6 +91,30 @@
         <script src="{{asset('frontend/assets/js/contact-form-script.js')}}"></script>
         <!-- Custom JS -->
         <script src="{{asset('frontend/assets/js/custom.js')}}"></script>
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+        <script>
+        @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type','success') }}"
+        switch(type){
+            case 'info':
+            toastr.info(" {{ Session::get('message') }} ");
+            break;
+        
+            case 'success':
+            toastr.success(" {{ Session::get('message') }} ");
+            break;
+        
+            case 'warning':
+            toastr.warning(" {{ Session::get('message') }} ");
+            break;
+        
+            case 'error':
+            toastr.error(" {{ Session::get('message') }} ");
+            break; 
+        }
+        @endif 
+        </script>
         
     </body>
 </html>
